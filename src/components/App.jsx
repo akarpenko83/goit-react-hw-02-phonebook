ef11ffca7c507ff024f083a351ab4e5c016a6662
Anton K <akarpenko83@gmail.com>
2023-05-23 12:40:25 +0300

    configed project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,13 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import Section from './phonebook/Phonebook';
+class App extends Component {
+    state = {
+        contacts: [],
+        name: '',
+    };
+
+    render() {
+        return <Section></Section>;
+    }
+}
+export default App;
---
+++ src/components/phonebook/Phonebook.jsx
@@ -0,0 +1,7 @@
+export default function Section() {
+    return (
+        <section title="">
+            <h1>Phonebook</h1>
+        </section>
+    );
+}
